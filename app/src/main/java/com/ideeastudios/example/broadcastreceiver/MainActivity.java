9ef4a1a8c4b2b66863b2d5847f257fedfae2ca21
alexandrute <teodoroviciwork@gmail.com>
2018-07-05 16:38:26 +0300

    code update

--- app/src/main/java/com/ideeastudios/example/broadcastreceiver/MainActivity.java
+++ app/src/main/java/com/ideeastudios/example/broadcastreceiver/MainActivity.java
@@ -2,13 +2,42 @@ package com.ideeastudios.example.broadcastreceiver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConnectivityChangeReceiver receiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //instantiate the Broadcast Receiver here.
+        receiver = new ConnectivityChangeReceiver();
+
+        //register the Broadcast Receiver on application onCreate.
+        registerReceiver(receiver, new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION));
+        Toast.makeText(this, "Broadcast Receiver registered!", Toast.LENGTH_LONG).show();
+        Log.i("BROADCAST", "Broadcast Receiver registered!");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        //unregister the Broadcast Receiver when the app is destroyed.
+        unregisterReceiver(receiver);
+        Toast.makeText(this, "Broadcast Receiver unregistered!", Toast.LENGTH_LONG).show();
+        Log.i("BROADCAST", "Broadcast Receiver unregistered!");
+    }
+
 }
